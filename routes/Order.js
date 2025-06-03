a2098e69d1cd66c96d4a265cd2077a808cd5c99b
harirajhari <hariraj121203@gmail.com>
2025-06-03 12:25:56 +0530

    orde bug fixed

--- routes/Order.js
+++ routes/Order.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/OrderController');
-const updateOrderStatus  = require('../controllers/OrderController');
-const getPendingOrders = require('../controllers/OrderController');
 const authenticateToken = require('../middleware/authMiddleware');
 
 // POST /api/orders → Create a new order
 router.post('/', authenticateToken, orderController.createOrder);
-router.patch('/orders/:orderId/status', updateOrderStatus);
-router.get('/orders/pending', getPendingOrders);
+router.patch('/orders/:orderId/status', orderController.updateOrderStatus);
+router.get('/orders/pending', orderController.getPendingOrders);
 
 module.exports = router;
